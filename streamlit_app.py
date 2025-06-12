80790b9a7ac25717490e3475762a9e88762aa20d
David L. <71908291+davesrunning@users.noreply.github.com>
2025-06-12 18:20:24 -0400

    Made headers smaller on first tab

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,7 +135,7 @@ with tab1:
     st.markdown('<div class="responsive-text"><p style="font-size:35px; color: #227e94">' \
     ' The RapidReach <sup><small>Patent Pending</small></sup></p></div>', unsafe_allow_html=True)
     st.write()
-    st.markdown('<p style="font-size:20px; color: #379991"> Device In Use </p>', unsafe_allow_html=True)
+    st.markdown('<p style="font-size:26px; color: #379991"> Device In Use </p>', unsafe_allow_html=True)
     video_path = Path("General Demonstration.mp4")
     video_bytes = video_path.read_bytes()
     encoded = base64.b64encode(video_bytes).decode()
@@ -161,7 +161,7 @@ with tab1:
         html(video_html, height=height_sc)
     else:
         st.warning("Waiting for screen dimensions... Please reload if nothing appears.")
-    st.markdown('<div class="responsive-text"> <p style="color: #379991;">VOC Interview Feedback on Previous Products</p></div>', unsafe_allow_html=True)
+    st.markdown('<div class="responsive-text"> <p style="font-size:26px; color: #379991;">VOC Interview Feedback on Previous Products</p></div>', unsafe_allow_html=True)
     col1, col2, col3 = st.columns(3)
     with col1:
         st.markdown('<p style="font-size:20px; color: #212529;">\"After doing something a lot you work around the limitations and stop thinking about it.\"</p>', unsafe_allow_html=True)
@@ -173,7 +173,7 @@ with tab1:
         st.markdown('<p style="font-size:20px; color: #212529;">\"Some retractors slip and you must keep moving around.\"</p>', unsafe_allow_html=True)
         st.markdown('<p style="font-size:14px; color: #212529;"> — Dr. Scarborough, MD </p>', unsafe_allow_html=True)
 
-    st.markdown('<p style="font-size:32px; color: #379991;"> User Feedback </p>', unsafe_allow_html=True)
+    st.markdown('<p style="font-size:26px; color: #379991;"> User Feedback </p>', unsafe_allow_html=True)
     col4, col5 = st.columns(2)
     with col4:
         st.markdown(f'<p style="font-size:20px; color: #212529;"> \"This is better than what\'s out there.\" </p>', unsafe_allow_html=True)
