f0901b3867ed6dabb3f596f640b1eb07bd4a9e84
RapidReach02 <rapidreach02@gmail.com>
2025-06-17 21:33:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -162,7 +162,7 @@ with tab1:
         st.markdown('<div class="responsive-text"><p style="color: #227e94; text-align: center;">' \
     '<strong>About The RapidReach Arm<sup style="font-size: 0.4em;">Patent Pending</small</sup></strong></p></div>', unsafe_allow_html=True)
     with col3:
-        st.image('RapidReach Arm.png', caption='Rapid Reach Arm', use_container_width=False, width=150)
+        st.image('RapidReach Arm.png', caption='Rapid Reach Arm', use_container_width=False, width=300)
     st.write()
     st.markdown('<div class="responsive-text2"><p style="color: #379991; text-align: center;"> Device In Use </p></div>', unsafe_allow_html=True)
     video_path = Path("General Demonstration.mp4")
